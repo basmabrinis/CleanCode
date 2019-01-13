e56e043b13e8e39cc49490e2d599b33742d2e810
Basma Brinis <basma2011@gmail.com>
2019-01-13 23:53:48 +0100

    [GIT]:CleanCode

---
+++ CleanCode/CleanCode/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+
+namespace CleanCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test FirstSolution!");
+            FirstSolution();
+
+            Console.WriteLine("Test FirstSolution!");
+            SecondSolution();
+
+            Console.ReadKey(true);
+
+
+        }
+
+        public static void FirstSolution()
+        {
+            object message = new MessageC();
+
+            if (message is MessageA)
+            {
+                var messageA = message as MessageA;
+                messageA?.MyCustomMethodOnA();
+            }
+            else if (message is MessageB)
+            {
+                var messageB = message as MessageB;
+                messageB?.MyCustomMethodOnB();
+                messageB?.SomeAdditionalMethodOnB();
+            }
+            else if (message is MessageC)
+            {
+                var messageC = message as MessageC;
+
+                messageC?.MyCustomMethodOnC();
+            }
+        }
+
+        private static void SecondSolution()
+        {
+            object message = new MessageC();
+            var t = message.GetType();
+
+            switch (message.GetType().Name)
+            {
+                case "MessageA":
+
+                    var messageA = message as MessageA;
+                    messageA?.MyCustomMethodOnA();
+                    break;
+
+                case "MessageB":
+                    var messageB = message as MessageB;
+                    messageB?.MyCustomMethodOnB();
+                    messageB?.SomeAdditionalMethodOnB();
+                    break;
+                case "MessageC":
+                    var messageC = message as MessageC;
+
+                    messageC?.MyCustomMethodOnC();
+                    break;
+
+            }
+        }
+    }
+
+    public class MessageA
+    {
+        public void MyCustomMethodOnA()
+        {
+            Console.WriteLine("Hello MyCustomMethodOnA!");
+        }
+    }
+
+    public class MessageB
+    {
+        public void MyCustomMethodOnB()
+        {
+            Console.WriteLine("Hello MyCustomMethodOnB!");
+        }
+        public void SomeAdditionalMethodOnB()
+        {
+            Console.WriteLine("Hello SomeAdditionalMethodOnB!");
+        }
+    }
+
+    public class MessageC
+    {
+        public void MyCustomMethodOnC()
+        {
+            Console.WriteLine("Hello MyCustomMethodOnC!");
+        }
+    }
+}
+
