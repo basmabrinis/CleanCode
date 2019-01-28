22a7ea4d9060b785ce790f723f9de7044d9a3590
basmabrinis <basma2011@gmail.com>
2019-01-28 17:24:47 +0100

    Application des remarques de Maximiliano

--- CleanCode/CleanCode/Program.cs
+++ CleanCode/CleanCode/Program.cs
@@ -1,98 +1,155 @@
-﻿using System;
-
-
-namespace CleanCode
+﻿namespace CleanCode
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// IMessage Interface
+    /// </summary>
+    public interface IMessage
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// MyCustomMethod Method
+        /// </summary>
+        /// <returns>string value</returns>
+        string MyCustomMethod();
+    }
+
+    /// <summary>
+    /// ExtendClass class
+    /// </summary>
+    public static class ExtendClass
+    {
+        /// <summary>
+        /// SomeAdditionalMethod Method
+        /// </summary>
+        /// <param name="message">IMessage message</param>
+        /// <returns>string value</returns>
+        public static string SomeAdditionalMethod(this IMessage message)
+        {
+            return "Hello SomeAdditionalMethod On class extentions!";
+        }
+    }
+
+    /// <summary>
+    /// MessageA class
+    /// </summary>
+    public class MessageA : IMessage
+    {
+        /// <summary>
+        /// MyCustomMethod method
+        /// </summary> 
+        /// <returns>string value</returns>
+        public string MyCustomMethod()
+        {
+            return "Hello MyCustomMethod On class MessageA!";
+        }
+    }
+
+    /// <summary>
+    /// MessageB class
+    /// </summary>
+    public class MessageB : IMessage
+    {
+        /// <summary>
+        /// MyCustomMethod Method
+        /// </summary>
+        /// <returns>string value</returns>
+        public string MyCustomMethod()
+        {
+            return "Hello MyCustomMethod On class MessageB!";
+        }
+    }
+
+    /// <summary>
+    /// MessageC class
+    /// </summary>
+    public class MessageC : IMessage
+    {
+        /// <summary>
+        /// MyCustomMethod Method
+        /// </summary>
+        /// <returns>string value</returns>
+        public string MyCustomMethod()
+        {
+            return "Hello MyCustomMethod On class MessageC!";
+        }
+    }
+
+    /// <summary>
+    /// Program Class for CleanCode application
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        public static void Main()
         {
             Console.WriteLine("Test FirstSolution!");
-            FirstSolution();
+            Console.WriteLine(FirstSolution());
 
-            Console.WriteLine("Test FirstSolution!");
-            SecondSolution();
+            Console.WriteLine("Test SecondSolution!");
+            Console.WriteLine(SecondSolution());
 
             Console.ReadKey(true);
-
-
         }
 
-        public static void FirstSolution()
+        /// <summary>
+        /// FirstSolution Method
+        /// </summary>
+        /// <returns>string value</returns>
+        private static string FirstSolution()
         {
-            object message = new MessageC();
-
-            if (message is MessageA)
+            try
             {
-                var messageA = message as MessageA;
-                messageA?.MyCustomMethodOnA();
-            }
-            else if (message is MessageB)
-            {
-                var messageB = message as MessageB;
-                messageB?.MyCustomMethodOnB();
-                messageB?.SomeAdditionalMethodOnB();
-            }
-            else if (message is MessageC)
-            {
-                var messageC = message as MessageC;
+                object message = new MessageA();
+                var resultValues = string.Empty;
 
-                messageC?.MyCustomMethodOnC();
-            }
-        }
-
-        private static void SecondSolution()
-        {
-            object message = new MessageC();
-            switch (message.GetType().Name)
-            {
-                case "MessageA":
-
+                if (message is MessageA)
+                {
                     var messageA = message as MessageA;
-                    messageA?.MyCustomMethodOnA();
-                    break;
-
-                case "MessageB":
+                    resultValues = messageA?.MyCustomMethod();
+                }
+                else if (message is MessageB)
+                {
                     var messageB = message as MessageB;
-                    messageB?.MyCustomMethodOnB();
-                    messageB?.SomeAdditionalMethodOnB();
-                    break;
-                case "MessageC":
+                    resultValues = messageB?.MyCustomMethod();
+                    resultValues = resultValues + messageB?.SomeAdditionalMethod();
+                }
+                else if (message is MessageC)
+                {
                     var messageC = message as MessageC;
 
-                    messageC?.MyCustomMethodOnC();
-                    break;
+                    resultValues = messageC?.MyCustomMethod();
+                }
 
+                return resultValues;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
             }
         }
-    }
 
-    public class MessageA
-    {
-        public void MyCustomMethodOnA()
+        /// <summary>
+        /// SecondSolution Method
+        /// </summary>
+        /// <returns>string value</returns>
+        private static string SecondSolution()
         {
-            Console.WriteLine("Hello MyCustomMethodOnA!");
-        }
-    }
+            try
+            {
+                var message = new MessageA();
 
-    public class MessageB
-    {
-        public void MyCustomMethodOnB()
-        {
-            Console.WriteLine("Hello MyCustomMethodOnB!");
-        }
-        public void SomeAdditionalMethodOnB()
-        {
-            Console.WriteLine("Hello SomeAdditionalMethodOnB!");
-        }
-    }
+                var resultValues = message.MyCustomMethod();
+                resultValues = resultValues + Environment.NewLine + message.SomeAdditionalMethod();
 
-    public class MessageC
-    {
-        public void MyCustomMethodOnC()
-        {
-            Console.WriteLine("Hello MyCustomMethodOnC!");
+                return resultValues;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
         }
     }
 }
-
